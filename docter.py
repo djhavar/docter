6444e2eda12d58175e8d8430cb36a120ab3db4a8
djhavar <90192071+djhavar@users.noreply.github.com>
2022-08-27 16:48:54 +0530

    Add files via upload

--- docter.py
+++ docter.py
@@ -7,27 +7,28 @@ class Doctor():
         bmi = weight/(height*height)
         print("your BMI is "+str(bmi))
         
-    def heart_rate(heart_rate):
-        if(heart_rate>60 and heart_rate<100):
+    def heart_rates(heart_rates):
+        if(heart_rates>60 and heart_rates<100):
             print("great your heart rate is normal")
             
         else:
             print("Your heart rate does not seem normal, please visit the clinic")
 
-class patient(Doctor):
-     def __init__(self,name,weight,height,heart_rate):
+class Patient(Doctor):
+     def __init__(self,name,weight,height,heart_rates):
          self.patient_name = name
          self.patient_weight = weight
          self.patient_height = height 
-         self.patient_heart_rate = heart_rate
+         self.patient_heart_rates = heart_rates
          
      def healthCheck(self):
           print("\n Patient Name"+self.patient_name)
           Doctor.BMI(self.patient_weight,self.patient_height)
-          Doctor.heart_rate(self.patient_heart_rate)
+          Doctor.heart_rates(self.patient_heart_rates)
           
-          patient1 = patient("michael", 30, 0.9144, 80)
-          patient1.healthCheck()
+patient1 = Patient("michael", 30, 0.9144, 80)
+patient1.healthCheck()
           
-          patient2 = patient("jesse", 40, 1, 120)
-          patient2.healthCheck()
+patient2 = Patient("jesse", 40, 1, 120)
+patient2.healthCheck()
+
